1356b3176a1a696e4465a791358fdc86ddf998c0
RohanKumar01 <rohankumar25601@gmail.com>
2024-02-04 20:22:09 +0530

    Projects section and hover animation

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+var fixed = document.querySelector("#fixed-image");
+var elemC = document.querySelector("#elem-container");
+
+elemC.addEventListener("mouseenter", function () {
+  fixed.style.display = "block";
+});
+
+elemC.addEventListener("mouseleave", function () {
+  fixed.style.display = "none";
+});
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    var image = e.getAttribute("data-image");
+    console.log(image);
+    fixed.style.backgroundImage = `url("${image}")`;
+  });
+});
